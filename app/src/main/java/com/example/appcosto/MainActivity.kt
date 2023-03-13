8871a78ce049426705af49b906bf677912917326
Hugo Avila <hugo.06@outlook.com>
2023-03-12 19:50:14 -0600

    agregar pantalla para formulario y funcion a boton para cambiar pantalla

--- app/src/main/java/com/example/appcosto/MainActivity.kt
+++ app/src/main/java/com/example/appcosto/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.appcosto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addProduct = findViewById<ImageButton>(R.id.addProduct)
+        addProduct.setOnClickListener{
+            val Intent = Intent(this, ProductNew::class.java)
+            startActivity(Intent)
+        }
     }
 }
